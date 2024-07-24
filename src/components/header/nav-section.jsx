b7d8c445a026d50c7a535f56672e9c28cfea9e9a
GereltuyaMz <gegiimz96@gmail.com>
2024-07-24 11:06:36 +0800

    add

--- src/components/header/nav-section.jsx
+++ src/components/header/nav-section.jsx
@@ -1,13 +1,20 @@
 import Menu from "./menu";
 import NavButtons from "./nav-buttons";
 
+const menuList = [
+	{ label: "Home", link: "/" },
+	{ label: "About", link: "/#about" },
+	{ label: "Work", link: "/#work" },
+	{ label: "Contact", link: "/#contact" },
+];
+
 const NavSection = () => {
-  return (
-    <div className="flex gap-6">
-      <Menu />
-      <NavButtons />
-    </div>
-  );
+	return (
+		<div className="flex gap-6">
+			<Menu menuList={menuList} />
+			<NavButtons />
+		</div>
+	);
 };
 
 export default NavSection;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,38 @@
+import ExperienceCard from "@/components/experiences";
+
+const experiences = [
+	{
+		title: "Senior",
+		companyLogo: "/img.png",
+		jobRoles: ["Devops", "Frontend", "Fullstack"],
+	},
+	{
+		title: "Team-Lead",
+		companyLogo: "/img.png",
+		jobRoles: ["Devops", "Frontend", "Fullstack"],
+	},
+	{
+		title: "Mid Engineer",
+		companyLogo: "/img.png",
+		jobRoles: ["Devops", "Frontend", "Fullstack"],
+	},
+	{
+		title: "Junior",
+		companyLogo: "/img.png",
+		jobRoles: ["Devops", "Frontend", "Fullstack"],
+	},
+];
+
 export default function Home() {
-  return (
-    <main>
-      <div className="red-text">Welcome</div>
-    </main>
-  );
+	return (
+		<main>
+			{/* {experiences.map(({ title, companyLogo, jobRoles }) => (
+				<ExperienceCard
+					title={title}
+					companyLogo={companyLogo}
+					jobRoles={jobRoles}
+				/>
+			))} */}
+		</main>
+	);
 }
